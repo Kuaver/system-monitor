a1f9a1cc783370ba0e6be1ca1087d9000ddd2c9b
Quentin Vorchak <quentin@vorchak.net>
2024-01-29 11:57:59 -0500

    Removed the multi-GPU coloring from graphs.py since GPUtil only works with Nvidia and PyADL doesn't seem to work at the moment.

--- src/graphs.py
+++ src/graphs.py
@@ -1,21 +1,10 @@
 #!/usr/bin/env python3
 
 import time
-from GPUtil import getGPUs
-
-GPU_NAME = getGPUs()[0].name
 
 def length_limiter(start_time):
     elapsed_time = time.time() - start_time
     return (0 if elapsed_time < 30 else elapsed_time - 30, max(elapsed_time, 30))
-
-def get_gpu_color(gpu_name):
-    color_map = {
-        "NVIDIA": "green",
-        "AMD": "red",
-        "INTEL": "blue"
-    }
-    return color_map.get(gpu_name.split()[0], "orange")
 
 def create_cpu_plot(fig_cpu, ax_cpu, timestamps, system_data, user_data, start_time):
     ax_cpu.clear()
@@ -37,5 +26,5 @@ def create_gpu_plot(fig_gpu, ax_gpu, timestamps, gpu_data, start_time):
     ax_gpu.set_xlim(xlim_values)
     ax_gpu.set_ylim([None, 100])
 
-    ax_gpu.plot(timestamps, gpu_data, label="GPU", color=get_gpu_color(GPU_NAME))
+    ax_gpu.plot(timestamps, gpu_data, label="GPU", color="green")
     ax_gpu.legend(loc="upper right", bbox_to_anchor=(1, 1))
